652b99c1ccf5d58f1b4facc6fa5270b338e84081
Toleti Mohana Supraja <toleti.supraja@sifycorp.com>
2026-01-22 19:05:16 +0530

    service name fix

--- sify/aiplatform/observability/openTelemetry/utils/service_name.py
+++ sify/aiplatform/observability/openTelemetry/utils/service_name.py
@@ -5,21 +5,21 @@ from pathlib import Path
 def detect_app_name() -> str:
  
  
-    if os.getenv("OTEL_SERVICE_NAME"):
-        return os.getenv("OTEL_SERVICE_NAME")
+    # if os.getenv("OTEL_SERVICE_NAME"):
+    #     return os.getenv("OTEL_SERVICE_NAME")
  
-    try:
-        entry = Path(sys.argv[0]).stem
-        if entry and entry not in {"python", "ipython"}:
-            return entry
-    except Exception:
-        pass
+    # try:
+    #     entry = Path(sys.argv[0]).stem
+    #     if entry and entry not in {"python", "ipython"}:
+    #         return entry
+    # except Exception:
+    #     pass
  
-    try:
-        cwd_name = Path.cwd().name
-        if cwd_name:
-            return cwd_name
-    except Exception:
-        pass
+    # try:
+    #     cwd_name = Path.cwd().name
+    #     if cwd_name:
+    #         return cwd_name
+    # except Exception:
+    #     pass
  
     return "sify-client-app"
