cb4ed0cb6cef5f69cf1023b72f6f3dc4b84f9b2a
Toleti Mohana Supraja <toleti.supraja@sifycorp.com>
2026-01-22 15:10:45 +0530

    service name fix

--- sify/aiplatform/observability/openTelemetry/utils/service_name.py
+++ sify/aiplatform/observability/openTelemetry/utils/service_name.py
@@ -16,24 +16,23 @@ def detect_app_name(default: str = "sify-client-app") -> str:
     service = os.getenv("OTEL_SERVICE_NAME")
     if service:
         return service
-    return default
 
 
     # 2. Entrypoint
-    # try:
-    #     entry = Path(sys.argv[0]).stem.lower()
-    #     if entry and entry not in INVALID_ENTRY_NAMES:
-    #         return entry
-    # except Exception:
-    #     pass
+    try:
+        entry = Path(sys.argv[0]).stem.lower()
+        if entry and entry not in INVALID_ENTRY_NAMES:
+            return entry
+    except Exception:
+        pass
 
-    # # 3. Current working directory
-    # try:
-    #     cwd = Path.cwd().name.lower()
-    #     if cwd:
-    #         return cwd
-    # except Exception:
-    #     pass
+    # 3. Current working directory
+    try:
+        cwd = Path.cwd().name.lower()
+        if cwd:
+            return cwd
+    except Exception:
+        pass
 
     # 4. Final fallback
-    # return default
+    return default
